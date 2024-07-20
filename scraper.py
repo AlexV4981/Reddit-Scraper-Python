3096f4c80ed7511c8581e9ccba16d1b8165f9ce3
Alex V3281 <145633682+AlexV4981@users.noreply.github.com>
2024-07-20 01:41:30 -0500

    Changing to Selenium JS as bs4 is unable access JS
    The end

--- scraper.py
+++ scraper.py
@@ -2,13 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 base_url = 'https://www.reddit.com'
-subreddit_name = 'TrueOffMyChest'
+subreddit_name = ''
 
 listings_url = f"{base_url}/r/{subreddit_name}/top/?t=week"
 
 response = requests.get(listings_url)
 if response.ok:
-    print("EEEEEEEEEE")
     soup = BeautifulSoup(response.content, 'html.parser')
 else:
     print(f'Error {response.status_code} while fetching listings URL')
@@ -23,8 +22,8 @@ individual_post_urls = []
 for link in top_20_links:
     individual_post_urls.append(base_url + link['href'])
 
+print(len(individual_post_urls))
 
-    
 
 
 for url in individual_post_urls:
